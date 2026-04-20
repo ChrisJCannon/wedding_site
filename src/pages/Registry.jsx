7c2ff29ccca7ee5aebc834b9372dc58c2e0ae6c4
Chris Cannon <chriscannononline@gmail.com>
2026-04-20 16:41:10 -0500

    fix

--- src/pages/Registry.jsx
+++ src/pages/Registry.jsx
@@ -1,4 +1,3 @@
-import { LinkPreview } from "@dhaiwat10/react-link-preview"
 export function Registry() {
     return (<>
         <div>We have an Amazon wishlist <b>here</b>, or if you'd like to contribute to our honeymoon fund you can do that via <b>PayPal</b> or <b>Venmo</b></div>
